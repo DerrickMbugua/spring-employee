fe8f467d39339c7be0ab183d379638d94f0e3192
Derrick Mwema <derrickmbugua50@gmail.com>
2022-06-02 12:53:05 +0300

    feat: save employee

--- src/main/java/com/example/employee/controller/EmployeeController.java
+++ src/main/java/com/example/employee/controller/EmployeeController.java
@@ -1,10 +1,13 @@
 package com.example.employee.controller;
 
+import com.example.employee.model.Employee;
 import com.example.employee.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class EmployeeController {
@@ -12,9 +15,24 @@ public class EmployeeController {
     private EmployeeService employeeService;
 
     //display all employees
-    @GetMapping
+    @GetMapping("/")
     public String viewHomePage(Model model){
         model.addAttribute("listEmployees", employeeService.getAllEmployees());
         return "index";
+    }
+
+    @GetMapping("/showNewEmployeeForm")
+    public String showNewEmployeeForm(Model model) {
+        //create model attribute to bind form data
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
+        return "new_employee";
+    }
+
+    @PostMapping("/saveEmployee")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee){
+        //save employee to db
+        employeeService.saveEmployee(employee);
+        return "redirect:/";
     }
 }
